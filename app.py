270c9a957bc702e7932c6ba37df91ff610db6251
michaelkolas <michael.kolas@mycampus.apus.edu>
2026-02-14 09:59:14 -0500

    Initial Flask app setup

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.get("/")
-def home():
-    return "Deployed from GitHub to Azure!"
+# Register blueprints here
+# from app.module import module_blueprint
+# app.register_blueprint(module_blueprint)
+
+if __name__ == '__main__':
+    app.run(debug=True)
